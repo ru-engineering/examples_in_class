1529a222f8844ac36a93259b92e67fe9b0d5c658
KjartanKlein <kjartanbklein@gmail.com>
2020-08-31 15:16:09 +0000

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+
+
 fn main() {
-    println!("Hello, world!");
+    let state = 3;
+
+    let error: RunningStates = match state {
+        1 => {
+            println!("The state is 1");
+            println!("this is a new line");
+            RunningStates::Ok
+        },
+        2 => {
+            println!("2");
+            RunningStates::UnexpectedValue(2)
+        },
+        x => RunningStates::LogicError(x)
+    };
+
+    match error {
+        RunningStates::Ok => println!("All ok"),
+        RunningStates::UnexpectedValue(y) => panic!("UNEXPECTED VALUE {}",y),
+        RunningStates::LogicError(z) => println!("Got something strange, {}",z),
+    }
+
+}
+
+
+pub enum RunningStates {
+    Ok,
+    UnexpectedValue(u8),
+    LogicError(u8),
+
+}
+
+pub enum Ip {
+    Ipv4(u8,u8,u8),
+    Ipv6(u8,u8),
 }
